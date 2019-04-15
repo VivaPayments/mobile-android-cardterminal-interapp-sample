4b4551492ba0553334244943c6360bb3bcc285a2
gthais <gthais@vivawallet.com>
2019-04-15 13:55:03 +0300

    first commit

---
+++ app/src/main/java/com/vivawallet/demopaymentapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.vivawallet.demopaymentapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText amountTxt;
+    EditText tipAmountTxt;
+    EditText refTxt;
+    EditText stanTxt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amountTxt = findViewById(R.id.amountTxt);
+        tipAmountTxt = findViewById(R.id.tipAmountTxt);
+        refTxt = findViewById(R.id.refTxt);
+        stanTxt = findViewById(R.id.stanTxt);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        deepLinkingLogic();
+
+    }
+
+    public void deepLinkingLogic(){
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+    }
+
+
+    public void saleAction(View v){
+
+    }
+
+    public void cancelTransactionAction(View v){
+
+    }
+
+}
